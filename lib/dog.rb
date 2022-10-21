3a3505ae39db8389136f427e7b5851ca64c7a3dc
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-21 18:05:12 +0300

    Completed assignment.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-
+    # Setter method
+    def name= dogs_name
+        @this_dogs_name = dogs_name # instance variable accessible to all methods within the class.
+    end
+    #Getter method
+    def name
+        @this_dogs_name
+    end
 end
